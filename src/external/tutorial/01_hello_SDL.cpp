1ded6a80c8ea94df1705b6f7b5287f19479abe66
Aniol Marín <aniol.marinatares@gamesrevolted.net>
2023-03-09 09:08:39 +0100

    strong typing. Compiling, not running

--- src/external/tutorial/01_hello_SDL.cpp
+++ src/external/tutorial/01_hello_SDL.cpp
@@ -1,9 +1,13 @@
 #include <SDL.h>
+
+module demo;
+
 import <iostream>;
 import <vector>;
 import <cstdlib>;
-#include <thread>
-#include <chrono>
+import <bitset>;
+import <thread>;
+import <chrono>;
 
 enum class ProgramStatus {
 	TERMINATE_OK,
@@ -11,76 +15,109 @@ enum class ProgramStatus {
 	RUNNING,
 };
 
-struct Point2 {
-	int x, y;
+using Uint1 = unsigned char;
+using Uint2 = unsigned short int;
+using Uint24 = std::bitset<24>;
+using Window = SDL_Window;
+using Surface = SDL_Surface;
+using Event = SDL_Event;
+using Time = std::chrono::system_clock::time_point;
+auto& Now{ std::chrono::system_clock::now };
+auto& TimeToUint{ std::chrono::system_clock::to_time_t };
+
+struct Point2D {
+	Uint16 x, y;
+};
+
+struct Screen {
+	const Uint2 w, h;
+	Screen(Uint2 width, Uint2 heigth) :
+		w{ width },
+		h{ heigth } {}
 };
 
 struct Color {
-	int r, g, b, a;
+	Uint8 r, g, b, a;
+	Uint24 rgb() const { return  r << sizeof(Uint8) * 2 + g << sizeof(Uint8) + b; }
+	Uint32 rgba() const { return a << sizeof(Uint8) * 3 + r << sizeof(Uint8) * 2 + g << sizeof(Uint8) + b; }
 };
 
 class Star {
-	Point2 position;
-	int speed;
-	double brightness;
+	Point2D position;
+	Uint1 speed;
+	Uint2 miliBrightness;
 	Color color;
-	void Reset(Point2 screenSize, int maxSpeed, bool initial) {
-		position.x = initial * (rand() % screenSize.x);
-		position.y = rand() % screenSize.y;
+	void Reset(const Screen& screen, const Uint1 maxSpeed, const bool initial) {
+		position.x = initial * (rand() % screen.w);
+		position.y = rand() % screen.h;
 		speed = 1 + rand() % maxSpeed;
-		brightness = (double)speed / (double)maxSpeed;
+		miliBrightness = (speed * 1000) / maxSpeed;
 		color.r = 0xFF - (rand() % 0x1F);
 		color.g = 0xFF - (rand() % 0x1F);
 		color.b = 0xFF - (rand() % 0x1F);
 		color.a = 0xFF;
 	}
 public:
-	Star(Point2 screenSize, int maxSpeed) {
+	Star(const Screen& screenSize, const int maxSpeed) {
 		Reset(screenSize, maxSpeed, true);
 	}
-	void Update(Point2 screenSize, int maxSpeed) {
+	void Update(const Screen& screen, const int maxSpeed) {
 		position.x += speed;
-		if (position.x >= screenSize.x) {
-			Reset(screenSize, maxSpeed, false);
+		if (position.x >= screen.w) {
+			Reset(screen, maxSpeed, false);
 		}
+	}
+
+	Uint24* getPixel(SDL_Surface* surface, int x, int y) {
+		return static_cast<Uint24*>(surface->pixels) + position.y * surface->pitch + x;
 	}
 
 	void Draw(SDL_Surface* screenSurface) {
 
 		int bpp = screenSurface->format->BytesPerPixel;
 
-		double trail{ brightness };
+		Uint2 trail{ miliBrightness };
 		int nextOffset{ 0 };
-		while (trail > 0.0) {
+		while (trail > 0) {
 
-			Uint8* pixel = (Uint8*)screenSurface->pixels + position.y * screenSurface->pitch + (position.x - nextOffset) * bpp;
+			Uint24* pixel{ getPixel(screenSurface, position.x - nextOffset, position.y) };
 			nextOffset++;
-			trail -= 0.01;
+			trail -= 100;
 
 			if (trail > 0 && position.x - nextOffset >= 0) {
-				pixel[0] = (Uint8)(color.b * trail);
-				pixel[1] = (Uint8)(color.g * trail);
-				pixel[2] = (Uint8)(color.r * trail);
+				*pixel = color.rgb();
 			}
 		}
 	}
 };
 
 struct Data {
-	const Point2 screenSize;
+	const Screen screen;
+	const Uint8 maxSpeed;
+	const Uint8 maxStars;
 	std::vector<Star> stars;
 	ProgramStatus status;
-	const int maxSpeed;
-	const int maxStars;
-	const int delayFPS;
-	SDL_Window* window;
-	SDL_Surface* surface;
-	SDL_Event e;
-	Data(Point2 screenSize, int speed, int stars, int fps) :
-		screenSize{ screenSize },
+	Uint8 deltaTime;
+	Window* window;
+	Surface* surface;
+	Event e;
+	Data(Point2D screenSize, Uint1 speed, Uint1 stars, Uint1 fps) :
+		screen{ screenSize.x, screenSize.y },
 		maxSpeed{ speed },
 		maxStars{ stars },
-		delayFPS{ 1000 / fps } {}
+		initialTime{ Now() },
+		msPerFrame{ (unsigned int)1000 / fps }
+	{}
+	int GetDeltaTime() const { return deltaTime; }
+	int GetNextDelayTime() const { return TimeToUint(Now()) - TimeToUint(previousTime); }
+	bool Running() const { return status == ProgramStatus::RUNNING; }
+	void SetNextFrameTime() {
+		previousTime = Now();
+	}
+private:
+	const Uint16 msPerFrame;
+	Time initialTime;
+	Time previousTime;
 };
 
 
@@ -94,10 +131,10 @@ void Synch(Data& data);
 
 int main(int argc, char* args[])
 {
-	Data data{ Point2{640, 480},10, 100, 60 };
+	Data data{ Point2D{640, 480},10, 100, 60 };
 	Init(data);
 
-	while (data.status == ProgramStatus::RUNNING) {
+	while (data.Running()) {
 		PollEvents(data);
 		Update(data);
 		Draw(data);
@@ -112,13 +149,13 @@ void Init(Data& data) {
 
 	if (SDL_Init(SDL_INIT_VIDEO) >= 0) {
 
-		data.window = SDL_CreateWindow("Stars", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, data.screenSize.x, data.screenSize.y, SDL_WINDOW_SHOWN);
+		data.window = SDL_CreateWindow("Stars", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, data.screen.w, data.screen.h, SDL_WINDOW_SHOWN);
 		data.surface = SDL_GetWindowSurface(data.window);
 
 		srand(0);
 
 		for (int i = 0; i < data.maxStars; i++) {
-			Star star{ data.screenSize, data.maxSpeed };
+			Star star{ data.screen, data.maxSpeed };
 			data.stars.push_back(star);
 		}
 	}
@@ -142,7 +179,7 @@ void PollEvents(Data& data) {
 void Update(Data& data) {
 
 	for (Star& star : data.stars) {
-		star.Update(data.screenSize, data.maxSpeed);
+		star.Update(data.screen, data.maxSpeed);
 	}
 }
 
@@ -157,5 +194,6 @@ void Draw(Data& data) {
 }
 
 void Synch(Data& data) {
-	std::this_thread::sleep_for(std::chrono::milliseconds(data.delayFPS));
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(data.GetNextDelayTime()));
 }
