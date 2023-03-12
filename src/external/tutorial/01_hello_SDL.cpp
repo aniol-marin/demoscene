5d27087a1fd474b534f49f8e4658f73f90dc34fe
amarin <aniol.marin@gmail.com>
2023-03-12 14:27:51 +0100

    adding ripped code from plasma

--- src/external/tutorial/01_hello_SDL.cpp
+++ src/external/tutorial/01_hello_SDL.cpp
@@ -17,6 +17,7 @@ enum class ProgramStatus {
 
 using Window = SDL_Window;
 using Surface = SDL_Surface;
+using Pixel = Uint32;
 using Event = SDL_Event;
 using Clock = std::chrono::steady_clock;
 using Time = Clock::time_point;
@@ -26,6 +27,8 @@ Uint16 delay(Time since, Time until) {
 	return (until - since).count() / 1000000; //TODO cast properly to milliseconds
 	//return std::chrono::duration<Uint16, ms>(until - since).count();
 }
+
+
 
 struct Point2D {
 	Uint16 x, y;
@@ -58,6 +61,13 @@ private:
 	}
 };
 
+Pixel* getPixel(SDL_Surface* surface, Uint16 x, Uint16 y) {
+	return reinterpret_cast<Uint32*>((Uint8*)surface->pixels + y * surface->pitch + x * surface->format->BytesPerPixel);
+}
+void putPixel(Pixel& pixel, const Color& color) {
+	pixel = color.rgba();
+}
+
 class Star {
 	Point2D position;
 	Uint8 speed;
@@ -84,11 +94,6 @@ public:
 		}
 	}
 
-	Uint32* getPixel(SDL_Surface* surface, int x, int y) {
-
-		return reinterpret_cast<Uint32*>((Uint8*)surface->pixels + position.y * surface->pitch + x * surface->format->BytesPerPixel);
-	}
-
 	void Draw(SDL_Surface* screenSurface) {
 
 		int trail{ miliBrightness };
@@ -98,6 +103,7 @@ public:
 			if (position.x - nextOffset >= 0) {
 				Uint32* pixel = getPixel(screenSurface, position.x - nextOffset, position.y);
 				*pixel = color.mLerp(trail);
+				//putPixel(*pixel, color.mLerp(trail));
 			}
 
 			trail -= 20;
@@ -106,11 +112,79 @@ public:
 	}
 };
 
+class Plasma {
+	long long accumulatedTime{};
+	std::vector<Uint8> plasma1;
+	std::vector<Uint8> plasma2;
+	int Windowx1, Windowy1, Windowx2, Windowy2;
+	long src1, src2;
+	Color palette[256];
+
+	void buildPalette(Uint16 deltaTime) {
+		for (int i = 0; i < 256; i++)
+		{
+			palette[i].r = (unsigned char)(128 + 127 * cos(i * M_PI / 128 + (double)deltaTime / 740));
+			palette[i].g = (unsigned char)(128 + 127 * sin(i * M_PI / 128 + (double)deltaTime / 630));
+			palette[i].b = (unsigned char)(128 - 127 * cos(i * M_PI / 128 + (double)deltaTime / 810));
+		}
+
+	}
+
+public:
+	void Init(const Screen& screen, Uint16 currentTime) {
+		buildPalette(currentTime);
+		for (int j = 0; j < screen.h * 2; ++j) {
+			for (int i = 0; i < screen.w * 2; ++i) {
+				plasma1.push_back(64 + 63 * (sin((double)hypot(screen.h - j, screen.w - i) / 16)));
+				plasma2.push_back((unsigned char)(64 + 63 * sin((float)i / (37 + 15 * cos((float)j / 74)))
+					* cos((float)j / (31 + 11 * sin((float)i / 57)))));
+			}
+		}
+	}
+
+	void updatePlasma(const Screen& screen, Uint16 currentTime) {
+		accumulatedTime += currentTime;
+		// setup some nice colours, different every frame
+		// this is a palette that wraps around itself, with different period sine
+		// functions to prevent monotonous colours
+		buildPalette(currentTime);
+
+		// move plasma with more sine functions :)
+		Windowx1 = (screen.w / 2) + (int)(((screen.w / 2) - 1) * cos((double)accumulatedTime / 970));
+		Windowx2 = (screen.w / 2) + (int)(((screen.w / 2) - 1) * sin((double)-accumulatedTime / 1140));
+		Windowy1 = (screen.h / 2) + (int)(((screen.h / 2) - 1) * sin((double)accumulatedTime / 1230));
+		Windowy2 = (screen.h / 2) + (int)(((screen.h / 2) - 1) * cos((double)-accumulatedTime / 750));
+		// we only select the part of the precalculated buffer that we need
+		src1 = Windowy1 * (screen.w * 2) + Windowx1;
+		src2 = Windowy2 * (screen.w * 2) + Windowx2;
+	}
+
+	void renderPlasma(Surface& surface, Screen screen) {
+
+		Uint32* pixel;
+		int indexColor;
+		for (Uint16 j = 0; j < screen.h; j++) {
+			for (Uint16 i = 0; i < screen.w; i++) {
+
+				pixel = getPixel(&surface, i, j);
+				indexColor = (plasma1[src1] + plasma2[src2]) % 256;
+				putPixel(*pixel, palette[indexColor]);
+
+				src1++; src2++;
+			}
+			// get the next line in the precalculated buffers
+			src1 += screen.w; src2 += screen.w;
+		}
+
+	}
+};
+
 struct Data {
 	const Screen screen;
 	const Uint8 maxSpeed;
 	const Uint8 maxStars;
 	std::vector<Star> stars;
+	Plasma plasma;
 	ProgramStatus status;
 	Window* window;
 	Surface* surface;
@@ -124,7 +198,7 @@ struct Data {
 		previousTime{ Now() }
 	{}
 	Uint16 GetDeltaTime() const { return deltaTime; }
-	ms GetNextDelayTime() { return  std::chrono::milliseconds{ 1000 / fps }; }//std::chrono::duration_cast<ms>(previousTime  - Now()) + frameTime; }
+	ms GetNextDelayTime() { return  std::chrono::milliseconds{ 1000 / fps }; }//std::chrono::duration_cast<ms>(previousTime - Now()) + frameTime; }
 	bool Running() const { return status == ProgramStatus::RUNNING; }
 	void SetNextFrameTime() {
 		deltaTime = delay(previousTime, Now());
@@ -169,13 +243,15 @@ void Init(Data& data) {
 
 		srand(0);
 
+		data.plasma.Init(data.screen, data.GetDeltaTime());
+
 		for (int i = 0; i < data.maxStars; i++) {
 			Star star{ data.screen, data.maxSpeed };
 			data.stars.push_back(star);
 		}
-	}
 
-	data.status = data.window != NULL ? ProgramStatus::RUNNING : ProgramStatus::TERMINATE_ERROR;
+		data.status = data.window != NULL ? ProgramStatus::RUNNING : ProgramStatus::TERMINATE_ERROR;
+	}
 }
 
 void Finalize(Data& data) {
@@ -207,14 +283,19 @@ void Update(Data& data) {
 	for (Star& star : data.stars) {
 		star.Update(data.screen, data.GetDeltaTime(), data.maxSpeed);
 	}
+	data.plasma.updatePlasma(data.screen, data.GetDeltaTime());
 }
 
 void Draw(Data& data) {
 
 	SDL_FillRect(data.surface, NULL, SDL_MapRGB(data.surface->format, 0x0, 0x5, 0x10));
+
+	SDL_LockSurface(data.surface);
+	data.plasma.renderPlasma(*data.surface, data.screen);
 	for (Star& star : data.stars) {
 		star.Draw(data.surface);
 	}
+	SDL_UnlockSurface(data.surface);
 
 	SDL_UpdateWindowSurface(data.window);
 }
