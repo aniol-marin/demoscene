1caf42a3e1cfb4ed0bf19969ae474e8f316b464f
Aniol Marín <aniol.marin@gmail.com>
2025-03-21 17:39:21 +0100

    [WIP-000] add more unique test cases 2025-03-21-1739

--- test/test.cpp
+++ test/test.cpp
@@ -36,9 +36,10 @@ TEST_CASE("Container supports unique instances", "[Injection]")
 	{
 		REQUIRE(true);
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<int, int>(); }));
-		/*
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_class>(); }));
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_derived_class>(); }));
+		/*
+		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_class>( test_class{}); }));
 		*/
 	}
 
