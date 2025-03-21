f400b35735830c0d89bd82afe6ed9297f37e4f1b
Aniol Marín <aniol.marin@gmail.com>
2025-03-21 18:18:51 +0100

    [WIP-000]  2025-03-21-1818

--- test/test.cpp
+++ test/test.cpp
@@ -41,8 +41,8 @@ TEST_CASE("Container supports unique instances", "[Injection]")
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_class>(); }));
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_derived_class>(); }));
 
-		/*
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<int, int>( []{ return int{}; } ); }));
+		/*
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_class>(); }));
 		REQUIRE_NOTHROW( std::invoke( [&] { c.BindUnique<test_class, test_derived_class>( []{ return test_derived_class{}; } ); }));
 		*/
