5bd471b254d1cc9d5c686a9810964778a996bea3
Aniol Marín <aniol.marin@gmail.com>
2025-05-02 17:00:14 +0200

    add test prototype for timer

---
+++ test/timer/test.cpp
@@ -0,0 +1,18 @@
+#include<catch2/catch_test_macros.hpp>
+
+import std;
+
+SCENARIO("Timer")
+{
+	GIVEN("a timer")
+	{
+		WHEN("instantiated")
+		{
+			THEN("test suite should work")
+			{
+				CHECK(true);
+			}
+		}
+	}
+}
+
