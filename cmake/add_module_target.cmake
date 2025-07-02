9b79c62e7c787f2b7663dedb0294b2d69fdcf6e5
Aniol Marín <aniol.marin@gmail.com>
2025-07-02 07:01:44 +0200

    bring backport back to generation

--- cmake/add_module_target.cmake
+++ cmake/add_module_target.cmake
@@ -2,4 +2,9 @@ function(add_module_target MODULE_NAME)
 
 	add_library(${MODULE_NAME})
 
+	target_include_directories( ${MODULE_NAME} PUBLIC
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+	)
+
+
 endfunction()
