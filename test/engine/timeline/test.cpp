9a06d01dc32a778618cf93c1920c95cebe7e8e22
Aniol Marín <aniol.marin@gmail.com>
2025-07-07 06:18:28 +0200

    recover unit test prototype for timeline

--- test/engine/timeline/test.cpp
+++ test/engine/timeline/test.cpp
@@ -1,25 +1,27 @@
-#include<catch2/catch_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 import std;
 import timer;
 
 SCENARIO("Timeline")
 {
-	GIVEN("a timeline")
-	{
-		WHEN("instantiated")
-		{
-			MoleDemo::Timer t{};
-			int initial { t. GetTime() };
-			std::cerr << "\n initial: [" << initial << "]";
-			std::cerr << "\n sleeping...";
-			std::this_thread::sleep_for(std::chrono::seconds{1});
-			t.WaitUntilNextFrame();
-			int final { t. GetTime() };
-			std::cerr << "\n final: [" << final << "]";
-			return final == initial + 1;
-		}));
-            }
+    GIVEN("a timeline")
+    {
+        WHEN("instantiated")
+        {
+            CHECK(std::invoke(
+                    []
+                    {
+                        MoleDemo::Timer t{};
+                        int initial{ t.GetTime() };
+                        std::cerr << "\n initial: [" << initial << "]";
+                        std::cerr << "\n sleeping...";
+                        std::this_thread::sleep_for(std::chrono::seconds{ 1 });
+                        t.WaitUntilNextFrame();
+                        int final{ t.GetTime() };
+                        std::cerr << "\n final: [" << final << "]";
+                        return final == initial + 1;
+                    }));
         }
     }
 }
