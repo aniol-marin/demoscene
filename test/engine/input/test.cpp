f0f72e1d3813666e7c4f2353936202d751519b25
Aniol Marín <aniol.marin@gmail.com>
2025-08-12 18:03:26 +0200

    recover input test prototype

--- test/engine/input/test.cpp
+++ test/engine/input/test.cpp
@@ -1,10 +1,8 @@
-#include<catch2/catch_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
 
-import input;
-import std.compat;
+#include "input.h"
 
 TEST_CASE("test", "[input]")
 {
-	REQUIRE(true);
+    REQUIRE(true);
 }
-
