b1f59f8a9a194651e077fe6a23224c47d9fff3ec
Aniol Marín <aniol.marin@gmail.com>
2025-08-13 06:58:45 +0200

    incremental changes on documentation

--- test/engine/program/test.cpp
+++ test/engine/program/test.cpp
@@ -1,8 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
 
-import std;
-import definitions;
-import program;
+#include "program.h"
+#include "definitions.h"
+#include <functional>
+#include <type_traits>
 
 SCENARIO("Program")
 {
