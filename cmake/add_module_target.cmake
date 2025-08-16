17ec0f4a26344a4afacd5081e92c1ef764a7f5b4
Aniol Marín <aniol.marin@gmail.com>
2025-08-16 12:20:23 +0200

    rethink module-header relationship, prototype transitional cmake script

--- cmake/add_module_target.cmake
+++ cmake/add_module_target.cmake
@@ -3,3 +3,15 @@ function(add_module_target MODULE_NAME)
 	add_library(${MODULE_NAME})
 
 endfunction()
+
+function(add_module_target_transitional MODULE_NAME)
+
+	add_library(${MODULE_NAME})
+
+	target_sources(${MODULE_NAME} PUBLIC
+		FILE_SET ${MODULE_NAME} TYPE CXX_MODULES
+		FILES
+		interface.cpp
+	)
+
+endfunction()
--- src/engine/effect/Effect.cpp
+++ src/engine/effect/Effect.cpp
@@ -1,46 +1,60 @@
-export module effect;
-
-import std;
-import definitions;
-import timer;
+module effect;
 
 namespace MoleDemo {
+	Effect::Effect(Timer* timer, Screen* screen) :
+		timer{ timer },
+		screen{ screen },
+		buffer{}
+	{
 
-	export class Effect;
+		ReserveBuffer();
+	}
+
+	Effect::~Effect() {};
+
+	index Effect::GetPixelIndex(Point2D& point) {
+		return GetPixelIndex(point, buffer);
+	}
+
+	index Effect::GetPixelIndex(Point2D& point, PixelBuffer& pixelBuffer) {
+		return (pixel_count)screen->w * (pixel_count)point.y + (pixel_count)point.x;
+	}
+
+	rgbaColor Effect::GetColorAt(Point2D point, PixelBuffer& pixelBuffer) {
+		return pixelBuffer[GetPixelIndex(point, pixelBuffer)];
+	}
+
+	void Effect::PutPixel(Point2D point, rgbaColor color) {
+		PutPixel(point, color, buffer);
+	}
+
+	void Effect::PutPixel(Point2D point, rgbaColor color, PixelBuffer& buffer) {
+
+		rgbaColor& pixel = buffer.at(GetPixelIndex(point));
+		pixel = color;
+	}
+
+	void Effect::ReserveBuffer() {
+		/*
+		buffer.reserve(screen->GetPixelCount());
+		*/
+		ClearBuffer(transparent);
+	}
+
+	void Effect::ClearBuffer(rgbaColor color) {
+		ClearBuffer(color, buffer);
+	}
+
+	void Effect::ClearBuffer(rgbaColor color, PixelBuffer& pixelBuffer) {
+		pixelBuffer.assign(screen->GetPixelCount(), color);
+	}
+
+	rgbaColor Effect::GetPixel(Point2D p) {
+		return GetPixel(GetPixelIndex(p));
+	}
+
+	rgbaColor Effect::GetPixel(index index) {
+		return buffer[index];
+	}
 }
-
-class MoleDemo::Effect {
-
-	PixelBuffer buffer;
-
-protected:
-	Timer* const timer;
-	Screen* const screen;
-
-	Effect(Timer* timer, Screen* screen);
-
-	index GetPixelIndex(Point2D& point);
-	index GetPixelIndex(Point2D& point, PixelBuffer& pixelBuffer);
-	rgbaColor GetColorAt(Point2D point, PixelBuffer& pixelBuffer);
-	void PutPixel(Point2D point, rgbaColor color);
-	void PutPixel(Point2D point, rgbaColor color, PixelBuffer& buffer);
-
-	void ReserveBuffer();
-	void ClearBuffer(rgbaColor color);
-	void ClearBuffer(rgbaColor color, PixelBuffer& pixelBuffer);
-
-public:
-
-	virtual ~Effect();
-
-	virtual rgbaColor GetPixel(Point2D p);
-	virtual rgbaColor GetPixel(index index);
-
-	virtual void Load() = 0;
-	virtual void Unload() = 0;
-
-	virtual void Update(permille intensity, milliseconds delta) = 0;
-
-	virtual void Cache(StencilBuffer& mask) = 0;
-};
 
