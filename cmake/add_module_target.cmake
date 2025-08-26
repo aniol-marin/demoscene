0dd52add30e700c84dba41eec5fe6b5d6939a5e3
Aniol Marín <aniol.marin@gmail.com>
2025-08-26 18:02:08 +0200

    additional tweaks to bring back to generation

--- cmake/add_module_target.cmake
+++ cmake/add_module_target.cmake
@@ -2,16 +2,28 @@ function(add_module_target MODULE_NAME)
 
 	add_library(${MODULE_NAME})
 
+	target_include_directories( ${MODULE_NAME} PUBLIC
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+	)
+
 endfunction()
 
 function(add_module_target_transitional MODULE_NAME)
 
 	add_library(${MODULE_NAME})
 
-	target_sources(${MODULE_NAME} PUBLIC
-		FILE_SET ${MODULE_NAME} TYPE CXX_MODULES
-		FILES
-		interface.cpp
+	target_include_directories( ${MODULE_NAME} PUBLIC
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+	)
+
+endfunction()
+
+function(add_module_target_no_implementation MODULE_NAME)
+
+	add_library(${MODULE_NAME} INTERFACE)
+
+	target_include_directories( ${MODULE_NAME} INTERFACE
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
 	)
 
 endfunction()
