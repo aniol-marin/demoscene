b41544c35a95f43c98ecabee28ab11a1bb73578b
Aniol Marín <aniol.marin@gmail.com>
2025-10-19 19:32:42 +0200

    heavy deactivation due to weird things happening, but glad glad is back to compilation

--- src/editor/dependencies/glad/glad.cpp
+++ src/editor/dependencies/glad/glad.cpp
@@ -4,39 +4,40 @@ import std;
 
 namespace mole::graphics
 {
-        RenderContext::RenderContext(WindowContext& window) : window{ window }
-        {
-            const bool glLoaded{ 0 != gladLoadGL(window.address) };
+	RenderContext::RenderContext(WindowContext& window) : window{ window }
+	{
+		/*
+		   const bool glLoaded{ 0 != gladLoadGL(window.address) };
 
-            if (!glLoaded)
-            {
-                std::cerr << "OpenGL renderer failed to load";
-                throw std::exception();
-            }
+		   if (!glLoaded)
+		   {
+		   std::cerr << "OpenGL renderer failed to load";
+		   throw std::exception();
+		   }
 
-            glDepthFunc(GL_LEQUAL);
-            glEnable(GL_DEPTH_TEST);
+		   glDepthFunc(GL_LEQUAL);
+		   glEnable(GL_DEPTH_TEST);
 
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            glEnable(GL_BLEND);
-        }
-        
+		   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		   glEnable(GL_BLEND);
+		   */
+	}
+
 	RenderContext::RenderContext(RenderContext&& tmp) : window{ tmp.window } {};
 
-        void RenderContext::Render()
-        {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            /*
-	       glClearColor(0.1, 0.1, 0, 1);
-	       for (const auto& r: drawCalls)
-	       {
-	       r.Draw();
-	       }
-       */
-        }
-
-	void RenderContext::Present()
+	void RenderContext::Render()
 	{
+		/*
+		   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		   */
+		/*
+		   glClearColor(0.1, 0.1, 0, 1);
+		   for (const auto& r: drawCalls)
+		   {
+		   r.Draw();
+		   }
+		   */
 	}
-    };
+
+	void RenderContext::Present() {}
 }
--- src/editor/dependencies/glad/interface.cpp
+++ src/editor/dependencies/glad/interface.cpp
@@ -1,13 +1,16 @@
 module;
 
+/*
 extern "C" {
 #define GLAD_IMPLEMENTATION
 #include "glad/gl.h"
 }
 
+ */
 export module glad_wrapper;
 
 import std;
+import glfw_wrapper;
 
 namespace mole::graphics
 {
