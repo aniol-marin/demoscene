335fd4df2daad3f2016d1f0d7dfbf1e9bc18a299
Aniol Marín <aniol.marin@gmail.com>
2025-11-08 20:08:51 +0100

    bring editor back to compilation (all real features disabled)

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -3,14 +3,18 @@ import std;
 
 int main()
 {
+	/*
     Context context;
+	 */
 
     std::cout << "[EDITOR] running\n";
+    /*
     while (context.is_active())
     {
         context.PollEvents();
         context.Draw();
     }
+     */
 
     std::cout << "[EDITOR] finalizing\n";
 }
