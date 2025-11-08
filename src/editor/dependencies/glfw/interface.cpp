01fb79fc18722f6daa98186324b6b88ad7a969d7
Aniol Marín <aniol.marin@gmail.com>
2025-11-08 20:18:37 +0100

    reactivate glfw logic

--- src/editor/dependencies/glfw/interface.cpp
+++ src/editor/dependencies/glfw/interface.cpp
@@ -28,22 +28,13 @@ namespace mole::graphics
     static std::map<int, std::vector<callback>> MouseCursorPosCallbacks;
     static Vector2 delta;
 
-/*
-    static void ErrorCallback(int code, const char* message);
-    static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-    static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-    static void MouseCursorPosCallback(GLFWwindow* window, double x, double y);
-
     export using proc_address = decltype(glfwGetProcAddress);
- */
     export struct WindowContext
     {
         GLFWwindow* window{ NULL };
         const window_size size;
         const std::string_view name;
-	/*
         proc_address* address;
-	*/
 
         WindowContext() = delete;
         WindowContext(window_size&& size, std::string_view name);
@@ -51,9 +42,7 @@ namespace mole::graphics
         WindowContext(WindowContext&&) = default;
         ~WindowContext();
 
-	/*
         void Swap() const { glfwSwapBuffers(window); }
-	 */
         void AddCallback(const char key, callback&& callback) { callbacks[key].emplace_back(std::move(callback)); }
         void PollEvents();
     };
