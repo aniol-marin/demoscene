94ff3ef4433f5e8d3e92d1c921c534c0c0874718
Aniol Marín <aniol.marin@gmail.com>
2025-11-30 11:38:55 +0100

    correct pugi required version

--- cmake/dependency_managers/manage_dependency_pugixml.cmake
+++ cmake/dependency_managers/manage_dependency_pugixml.cmake
@@ -1,22 +1,26 @@
 function(manage_dependency_pugixml)
 
-	FetchContent_Declare(
-		pugixml
-		EXCLUDE_FROM_ALL
-		GIT_REPOSITORY https://github.com/zeux/pugixml.git
-		GIT_TAG ee86beb30e4973f5feffe3ce63bfa4fbadf72f38 # v 1.15
-		FIND_PACKAGE_ARGS NAMES pugixml
-	)
-	FetchContent_MakeAvailable(pugixml)
+	find_package(pugixml 1.15)
 
-	mark_as_advanced(FORCE
-		FETCHCONTENT_SOURCE_DIR_PUGIXML
-		FETCHCONTENT_UPDATES_DISCONNECTED_PUGIXML
-		PUGIXML_BUILD_DEFINES
-		PUGIXML_COMPACT
-		pugixml_DIR
-		PUGIXML_INSTALL
-		PUGIXML_WCHAR_MODE
-	)
+	if(NOT pugixml_FOUND)
+		FetchContent_Declare(
+			pugixml
+			EXCLUDE_FROM_ALL
+			GIT_REPOSITORY https://github.com/zeux/pugixml.git
+			GIT_TAG ee86beb30e4973f5feffe3ce63bfa4fbadf72f38 # v 1.15
+			FIND_PACKAGE_ARGS NAMES pugixml
+		)
+		FetchContent_MakeAvailable(pugixml)
+
+		mark_as_advanced(FORCE
+			FETCHCONTENT_SOURCE_DIR_PUGIXML
+			FETCHCONTENT_UPDATES_DISCONNECTED_PUGIXML
+			PUGIXML_BUILD_DEFINES
+			PUGIXML_COMPACT
+			pugixml_DIR
+			PUGIXML_INSTALL
+			PUGIXML_WCHAR_MODE
+		)
+	endif()
 
 endfunction()
