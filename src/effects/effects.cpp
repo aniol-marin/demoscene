ab777ff72fac47d790b5edc7c015ac9a9dc9caf5
Aniol Marín <aniol.marin@gmail.com>
2025-12-07 15:46:27 +0100

    recover missing effects interface, add glfw3 to enforcably fetched list

--- src/effects/effects.cpp
+++ src/effects/effects.cpp
@@ -1,14 +1,2 @@
-export module effects;
+module effects;
 
-export import effect;
-export import stars;
-/*
-export import solid;
-export import gradient;
-export import wheel;
-export import chess;
-export import plasma;
-export import noise;
-export import tunel;
-export import fire;
- */
