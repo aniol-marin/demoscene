3d65da61b2298e2d9f30e8fed00669b30b3ab0bd
Aniol Marín <aniol.marin@gmail.com>
2025-12-27 12:29:11 +0100

    deactivate only modern implementation of timer

--- src/engine/timer/timer.cpp
+++ src/engine/timer/timer.cpp
@@ -1,8 +1,5 @@
 #include "timer.h"
-
-#include <chrono>
-#include <thread>
-#include <cstdint>
+#include <exception>
 
 namespace MoleDemo
 {
@@ -19,72 +16,113 @@ namespace MoleDemo
      {
      return function(std::forward<Args>(args)...);
      }
-     */
     using namespace mole_def;
     using mole_ms = mole_def::milliseconds;
     using mole_s = mole_def::seconds;
     using s = std::chrono::seconds;
+    */
 
-    Timer::Timer() :
-      initial_time{ clock_t::now() }, //
-      last_time{ initial_time },
-      last_mark{ initial_time }
+    Timer::Timer()
+	    : initial_time()
+	    , last_time()
+	    , last_mark()
+	    , delta_time()
+	    , frameTime()
+	    /*
+	    : initial_time{ clock_t::now() }
+	    , last_time{ initial_time }
+	    , last_mark{ initial_time }
+	     */
     {
     }
 
+    Timer::Timer(const Timer&)
+	    : initial_time()
+	    , last_time()
+	    , last_mark()
+	    , delta_time()
+	    , frameTime()
+    {
+	    throw std::exception();
+    }
+
+    Timer::~Timer(){}
+
     void Timer::update_frame_level_state()
     {
+        time_t current_time = 0;
+	    /*
         time_t current_time{ clock_t::now() };
         delta_time = static_cast<mole_ms>(std::chrono::duration_cast<ms>(current_time - last_time).count());
+	     */
         last_time = current_time;
         ++frames_since_mark;
     }
 
     ms Timer::get_next_time_target() const
     {
+	    /*
         return std::chrono::duration_cast<std::chrono::milliseconds>(last_time + frameTime - clock_t::now());
+	*/
+	    return 0;
     }
 
-    milliseconds Timer::GetDeltaTime() const
+    mole_ms Timer::GetDeltaTime() const
     {
         return delta_time;
     }
 
-    seconds Timer::get_total_time() const
+    mole_s Timer::get_total_time() const
     {
+	    /*
         return static_cast<seconds>(std::chrono::duration_cast<std::chrono::seconds>(endTime - initial_time).count());
+	     */
+	    return 0;
     }
 
-    void Timer::SetFPS(std::uint_fast8_t fps)
+    void Timer::SetFPS(fps_t fps)
     {
-        frameTime = ms{ permilleFactor / fps };
+        frameTime = static_cast<ms>( mole_def::permilleFactor() / fps );
     }
 
-    void Timer::SetEndTime(seconds seconds)
+    void Timer::SetEndTime(mole_s seconds)
     {
+	/*
         endTime = initial_time + std::chrono::seconds(seconds);
+	*/
     }
 
     void Timer::WaitUntilNextFrame()
     {
+	    /*
         std::this_thread::sleep_for(get_next_time_target());
         update_frame_level_state();
+	     */
     }
 
     void Timer::set_mark()
     {
-        frames_since_mark = {};
+        frames_since_mark = 0;
+	/*
         last_mark = { clock_t::now() };
+	 */
     }
 
     mole_s Timer::GetTime() const
     {
+	    /*
         return static_cast<mole_s>(std::chrono::duration_cast<s>(last_time - initial_time).count());
+	     */
+	return 0;
     }
 
     bool Timer::EndReached() const
     {
+
+	    /*
         return clock_t::now() >= endTime;
+	     */
+	    return true;
     }
 
     frame_t Timer::get_frames_since_mark() const
--- src/engine/timer/timer.h
+++ src/engine/timer/timer.h
@@ -1,8 +1,7 @@
 #ifndef MOLE_TIMER_H
 #define MOLE_TIMER_H
 
-#include <chrono>
-#include <cstdint>
+#include <ctime>
 
 #include "definitions.h"
 
@@ -10,6 +9,7 @@ namespace MoleDemo
 {
     class Timer;
 
+    /*
     using clock_t = std::chrono::steady_clock;
     using time_t = clock_t::time_point;
 
@@ -17,6 +17,14 @@ namespace MoleDemo
     using mole_s = mole_def::seconds;
     using mole_ms = mole_def::milliseconds;
     using ms = std::chrono::milliseconds;
+     */
+    typedef unsigned char fps_t;
+    typedef long unsigned int ms;
+    typedef long unsigned int time_t;
+    typedef long unsigned int frame_t;
+    typedef long unsigned int mole_ms;
+    typedef long unsigned int mole_s;
+    typedef std::clock_t clock_t;
 }
 
 class MoleDemo::Timer
@@ -24,23 +32,22 @@ class MoleDemo::Timer
     const time_t initial_time;
     time_t last_time;
     time_t last_mark;
-    mole_ms delta_time{};
-    ms frameTime{};
+    mole_ms delta_time;
+    ms frameTime;
 
     time_t endTime;
-    frame_t frames_since_mark{};
+    frame_t frames_since_mark;
 
     void update_frame_level_state();
     ms get_next_time_target() const;
 
 public:
     Timer();
-    Timer(const Timer&) = delete;
-    Timer(Timer&&) = default;
-    ~Timer() = default;
+    Timer(const Timer&);
+    ~Timer();
 
     mole_ms GetDeltaTime() const;
-    void SetFPS(std::uint_fast8_t fps);
+    void SetFPS(fps_t fps);
     void SetEndTime(mole_s mole_s);
     void set_mark();
     void WaitUntilNextFrame();
