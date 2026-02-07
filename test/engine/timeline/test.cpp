92dd90e805d8276326919aaf8c4307077b27c6d9
Aniol Marín <aniol.marin@gmail.com>
2026-02-07 09:18:30 +0100

    move CMAKE_ variable to presets. Fix broken tests.

--- test/engine/timeline/test.cpp
+++ test/engine/timeline/test.cpp
@@ -70,9 +70,11 @@ SCENARIO("Timeline")
 					MoleDemo::RenderManager render_manager { screen, sdl_manager };
 					MoleDemo::Cycle cycle { program, timer, input_manager, render_manager };
 					MoleDemo::SoundManager sound_manager { };
+					mole_def::PixelBuffer buffer {};
 
 					MoleDemo::Timeline _ {
 						timer,
+						buffer,
 						program,
 						cycle,
 						sound_manager,
@@ -92,9 +94,11 @@ SCENARIO("Timeline")
 			MoleDemo::RenderManager render_manager { screen, sdl_manager };
 			MoleDemo::Cycle cycle { program, timer, input_manager, render_manager };
 			MoleDemo::SoundManager sound_manager { };
+			mole_def::PixelBuffer buffer {};
 
 			MoleDemo::Timeline timeline {
 				timer,
+				buffer,
 				program,
 				cycle,
 				sound_manager,
